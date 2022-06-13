930fdb4f5e9a39ae161a2523c9805efde2843afc
konin.sergei@gmail.com <konin.sergei@gmail.com>
2022-06-13 23:40:20 +0300

    Return null section

--- lib/repositories/menu_repository.dart
+++ lib/repositories/menu_repository.dart
@@ -7,7 +7,8 @@ import '../models/menu_model.dart';
 class MenuRepository {
   Future<Data?> fetchMenu() async {
     var dio = Dio();
-    final response = await dio.get('https://gist.githubusercontent.com/pabushabi/d0ae94f936156055b0d22a40c9979e0b/raw/1e11b17b3dac9ffb056afa4240db94f5cc3bac4a/menu.json');
+    final response = await dio.get(
+        'https://gist.githubusercontent.com/pabushabi/d0ae94f936156055b0d22a40c9979e0b/raw/1e11b17b3dac9ffb056afa4240db94f5cc3bac4a/menu.json');
     var jsonMapData = jsonDecode(response.data);
     var data = Data.fromJson(jsonMapData);
     return data;
@@ -19,11 +20,15 @@ class MenuRepository {
 class CategoryRepository {
   Future<Categories?> fetchCategory({required int category_id}) async {
     var dio = Dio();
-    final response = await dio.get('https://gist.githubusercontent.com/pabushabi/d0ae94f936156055b0d22a40c9979e0b/raw/1e11b17b3dac9ffb056afa4240db94f5cc3bac4a/menu.json');
+    final response = await dio.get(
+        'https://gist.githubusercontent.com/pabushabi/d0ae94f936156055b0d22a40c9979e0b/raw/1e11b17b3dac9ffb056afa4240db94f5cc3bac4a/menu.json');
     var jsonMapData = jsonDecode(response.data);
     var data = Data.fromJson(jsonMapData);
 
-    return data.categories![category_id];
-    return null;
+    if (data.categories == null) {
+      return null;
+    } else {
+      return data.categories![category_id];
+    }
   }
 }
